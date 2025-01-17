77da771fdd23a24a68d4069dc37f2877d49b75a5
Kiaran Lau <kiaranlau@eduroam-169-233-211-112.ucsc.edu>
2025-01-16 17:06:23 -0800

    first commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('postcss-load-config').Config} */
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
 const config = {
   plugins: {
+    autoprefixer: {},
     tailwindcss: {},
   },
 };
