3789e49a37982289084fac20c240933b5cb8aa0f
Kiaran Lau <kiaranlau@eduroam-169-233-252-50.ucsc.edu>
2025-01-17 13:24:02 -0800

    changing top margin

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Image from 'next/image';
+
+const HeroSection = () => {
+  return (
+    <section>
+        <div className="grid grid-cols-1 lg:grid-cols-12">
+            <div className="col-span-7 place-self-center">
+                <h1 className="text-white mb-4 text-6xl font-extrabold">Hello, I'm Kiaran</h1>
+                <p className="text-[#BFBFBF] text-lg">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                </p>
+            </div>
+            <div className="col-span-5 place-self-center">
+                <div className="rounded-full bg-[#202020] my-10px w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+                    <Image
+                        className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 center rounded-3xl"
+                        src="/images/me_tidepools.jpg"
+                        alt="Myself at the JSA tidepools event in Fall 2024, photo taken by Kyle."
+                        width={500}
+                        height={500}
+                    />
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <h1>portfolio</h1>
+    <main className="flex min-h-screen flex-col bg-[#121212] container px-12 py-4 mx-auto">
+      <HeroSection/>  
     </main>
   );
 }
